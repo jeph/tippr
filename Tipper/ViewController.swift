26b8e89112f20de6f0444120aeb4a24e20985d9b
Jeph <jephyliu@gmail.com>
2017-12-25 16:31:38 -0500

    Added app icon and features

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.billField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let greyColor = UIColor(displayP3Red: 199.0/255.0, green: 199.0/255.0, blue: 204.0/255.0, alpha: 1)
+        
+        let tipPercentages = [0.15,0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        if(tip == 0){
+            tipLabel.textColor = greyColor
+            totalLabel.textColor = greyColor
+            tipLabel.text = "Tip"
+            totalLabel.text = "Total"
+        } else {
+            tipLabel.textColor = UIColor.black
+            totalLabel.textColor = UIColor.black
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        }
+        
+    }
+    
 }
 
